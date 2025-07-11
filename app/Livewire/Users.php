ec4fdbd2a535f8a7bcf8c5ce95913958161b6b7a
Cahya Apriyana <cahyaim3@gmail.com>
2025-07-11 20:41:03 +0700

    Add avatar upload functionality to user creation

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -6,11 +6,13 @@ use App\Models\User;
 use Livewire\Component;
 use Livewire\Attributes\Validate;
 use Illuminate\Support\Facades\Hash;
-
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 
 class Users extends Component
 {
 
+    use WithFileUploads;
+    
     #[Validate('required|min:3')]
     public $name = '';
     
@@ -20,25 +22,24 @@ class Users extends Component
     #[Validate('required|min:3')]
     public $password = '';
     
+    #[Validate('image|max:5000')]
+    public $avatar;
 
     public function createNewUser() {
 
-        // // $validated = $this->validate([
-
-        //     // 'name' => 'required|min:3',
-        //     // 'email' => 'required|email:dns|unique:users',
-        //     // 'password' => 'required|min:3',
 
 
-        // ]);
+       $validated = $this->validate();
 
-        $this->validate(); 
-        
+       if($this->avatar){
+        $validated['avatar'] = $this->avatar->store('avatar', 'public');
+       }
 
        User::create([
         'name' => $this->name,
         'email' => $this->email,
-        'password' => Hash::make($this->password)
+        'password' => Hash::make($this->password),
+        'avatar' => $validated['avatar'] 
        ]);
 
        $this->reset();
