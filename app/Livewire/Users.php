1be8481a7275f630b0af8d1ce8dabcfd1699f9ef
Cahya Apriyana <cahyaim3@gmail.com>
2025-07-11 20:02:51 +0700

    livewire validated

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -4,16 +4,37 @@ namespace App\Livewire;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\Attributes\Validate;
 use Illuminate\Support\Facades\Hash;
+
 
 class Users extends Component
 {
 
+    #[Validate('required|min:3')]
     public $name = '';
+    
+    #[Validate('required|email:dns|unique:users')]
     public $email = '';
+
+    #[Validate('required|min:3')]
     public $password = '';
+    
 
     public function createNewUser() {
+
+        // // $validated = $this->validate([
+
+        //     // 'name' => 'required|min:3',
+        //     // 'email' => 'required|email:dns|unique:users',
+        //     // 'password' => 'required|min:3',
+
+
+        // ]);
+
+        $this->validate(); 
+        
+
        User::create([
         'name' => $this->name,
         'email' => $this->email,
@@ -21,6 +42,8 @@ class Users extends Component
        ]);
 
        $this->reset();
+       session()->flash('success', 'User successfully Created.');
+
     }
 
 
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,22 +1,37 @@
 <div class="w-1/2 m-auto my-10">
 <div class="py-2">
+  @if (session('success'))
+    
   <div class="sm:mx-auto">
     <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Create New User</h2>
+    <div class="p-4 mt-6 mb-4 text-sm text-green-800 rounded-lg bg-green-50 " role="alert">
+      <span class="font-medium">{{ session('success') }}</span> 
+    </div>
   </div>
+  @endif
 
-  <div class="mt-10">
+  <div class="mt-4">
     <form class="space-y-6" action="#" method="POST" wire:submit.prevent="createNewUser">
         <div>
             <label for="name" class="block text-sm/6 font-medium text-gray-900">Name</label>
             <div class="mt-2">
               <input wire:model="name" type="text" name="name" id="name" autocomplete="name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+              @error('name')
+                
+              <p class="mt-2 text-xs text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</p>
+              @enderror
+
             </div>
           </div>
 
       <div>
         <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
         <div class="mt-2">
-          <input wire:model="email" type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          <input wire:model="email" type="email" name="email" id="email" autocomplete="email"   class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          @error('email')
+                
+          <p class="mt-2 text-xs text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</p>
+          @enderror
         </div>
       </div>
 
@@ -26,12 +41,16 @@
        
         </div>
         <div class="mt-2">
-          <input wire:model="password" type="password" name="password" id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          <input wire:model="password" type="password" name="password" id="password" autocomplete="current-password"   class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+          @error('password')
+                
+          <p class="mt-2 text-xs text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</p>
+          @enderror
         </div>
       </div>
 
       <div>
-        <button class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+        <button class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create new User</button>
       </div>
     </form>
 
