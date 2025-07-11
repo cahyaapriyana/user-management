d42139f7b96260c78dfe4e935cab359d47f0dd8b
Cahya Apriyana <cahyaim3@gmail.com>
2025-07-11 12:00:45 +0700

    Add user creation functionality and enhance user list display in Livewire component

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -4,11 +4,19 @@ namespace App\Livewire;
 
 use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Hash;
 
 class Users extends Component
 {
 
-    // public $title = 'User Page';
+    public function createUser() {
+       User::create([
+        'name' => 'User Baru 3',
+        'email' => 'tes12t@email.com',
+        'password' => Hash::make('password')
+       ]);
+    }
+
 
     public function render()
     {
