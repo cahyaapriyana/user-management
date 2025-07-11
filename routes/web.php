dbc15ce32c31ca8673668546e1e8b6c17e90a0cd
Cahya Apriyana <cahyaim3@gmail.com>
2025-07-11 11:44:42 +0700

    Add Livewire dependency and update routes for counter and users

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Livewire\Counter;
+use App\Livewire\Users;
 use Illuminate\Support\Facades\Route;
+
+use function Termwind\render;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/counter', Counter::class);
+
+Route::get('/users', function () {
+    return view('users');
 });
